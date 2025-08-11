9e99849b152dc4e54b2ffa4f1be36ee8cf839130
Samo Borzatta <samo.borzatta@gmail.com>
2025-08-11 13:26:39 +0200

    Konec še z zadnjo vajo

--- coins.py
+++ coins.py
@@ -2,11 +2,17 @@ import matplotlib.pyplot as plt
 import random
 from statistics import mean
 
-tosses = [random.randint(0, 1) for _ in range(10000)]
+PONOVITVE = 100000
+
+tosses = [random.randint(0, 1) for _ in range(PONOVITVE)]
 
 mean_value = mean(tosses)
+heads = 0
 
-print(mean_value)
-print(f"Odstopanje: {round(abs(0.5-mean_value)*100, 2)} %")
-plt.hist(tosses)
-plt.show()
+for x in range(int(len(tosses)/5)):
+    print(tosses[x*5:x*5+5])
+    if tosses[x*5:x*5+5].count(0) == 3:
+        heads += 1
+    
+print(f"Verjetnost, da pade glava je: {round(heads/(PONOVITVE/5)*100, 2)} %")
+print(f"Binosmka verjetnost: {round(10/(2**5)*100, 2)} %")
